c7c513267e37cc10e602ba97a66719fc0f0ab9f9
Mark Duell <markduell@amac02v7096htdg.connect>
2018-03-14 16:42:50 +0000

    fix style errors

--- features/steps/logging.py
+++ features/steps/logging.py
@@ -1,27 +1,27 @@
 from utils import logger
 import datetime
 
-@given(u'I have a "{message}" of type , "{level}" to log') # noqa
+
+@given(u'I have a "{message}" of type , "{level}" to log')  # noqa
 def step_impl(context, message, level):
     context.message = message
     context.level = level
 
 
-@when(u'the message is sent to standard output') # noqa
+@when(u'the message is sent to standard output')  # noqa
 def step_impl(context):
     log = logger('logs/test.log')
     log.log_message(context.message, context.level)
 
 
-@then(u'message is routed to the correct location') # noqa
+@then(u'message is routed to the correct location')  # noqa
 def step_impl(context):
     with open('logs/test.log', 'r') as file:
         context.log_content = file.read()
     assert context.message in context.log_content
 
 
-
-@then(u'the message has the correct logging level') # noqa
+@then(u'the message has the correct logging level')  # noqa
 def step_impl(context):
     with open('logs/test.log', 'r') as f:
         t = f.readlines()
@@ -33,7 +33,7 @@ def step_impl(context):
             assert i[1] == context.level
 
 
-@then(u'the message has the correct datetime stamp') # noqa
+@then(u'the message has the correct datetime stamp')  # noqa
 def step_impl(context):
     timestamp = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
     with open('logs/test.log', 'r') as f:
